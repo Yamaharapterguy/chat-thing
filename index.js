ad61d556f13b007e469f44deabcd2afc459a1ee8
Yamaharapterguy <109768199+Yamaharapterguy@users.noreply.github.com>
2023-05-16 08:04:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ const { render } = server.reply;
 server([
   get('/', ctx => render('index.html'))
 ]);
+
+const server = require('server');
+const { get, socket } = server.router;
+const { render } = server.reply;
+
+const updateCounter = ctx => {
+  ctx.io.emit('count', ctx.io.sockets.sockets.length);
+};
+
+server([
+  // For the initial load render the index.html
+  get('/', ctx => render('index.html')),
+
+  // Join/leave the room
+  socket('connect', updateCounter),
+  socket('disconnect', updateCounter)
+]);
